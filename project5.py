ec020fbcc720137bb27a176c220a0dc7d1999d8c
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2018-11-10 23:18:53 +0700

    start

--- project5.py
+++ project5.py
@@ -4,4 +4,15 @@
 кол-во палиндромных строк в тексте и сами строки.
 """
 
+import string
 
+bad_chars = string.whitespace + string.punctuation
+count = 0
+
+file_start = input('Введите имя файла: ')
+
+try:
+    input_file = open(file_start, 'r')
+except FileNotFoundError:
+    print('Файл {} не найден'.format(file_start))
+    exit()
